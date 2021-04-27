a237b34b4a5bfc39fe48b3997d380ab18f981676
alvations <alvations@gmail.com>
2021-04-27 18:28:11 -0400

    added script support instead of lang

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'spirit-guess',
   packages = find_packages(),
-  version = '0.0.6',
+  version = '0.0.7',
   description = '',
   long_description = '',
   author = 'Liling Tan',
--- spirit_guess/orthography.py
+++ spirit_guess/orthography.py
@@ -4,7 +4,7 @@ from collections import Counter
 
 from spirit_guess.languages import unicode_block
 
-def count_chars_in_blocks(text: str, normalize=True, remove_unknown=False) -> Counter:
+def count_chars_in_blocks(text: str, option='lang', normalize=True, remove_unknown=False) -> Counter:
     # Initialize counter.
     block_counts = Counter()
     for ch in text:  # Iterate through each character.
@@ -13,7 +13,7 @@ def count_chars_in_blocks(text: str, normalize=True, remove_unknown=False) -> Co
         try:
             # For some reason, the original code shifts to right by 4 bits
             # See https://stackoverflow.com/a/8345671/610569
-            block_counts.update(unicode_block(ord(ch) >> 4))
+            block_counts.update(unicode_block(ord(ch) >> 4, option=option))
         except (TypeError, KeyError):
             block_counts[{'unknown'}] += 1
     # Normalize ths score.
