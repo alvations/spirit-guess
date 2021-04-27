a0a24d5bcb453ebc5f2d8995d37bb775c193112c
alvations <alvations@gmail.com>
2021-04-27 18:43:54 -0400

    up version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'spirit-guess',
   packages = find_packages(),
-  version = '0.0.12',
+  version = '0.0.13',
   description = '',
   long_description = '',
   author = 'Liling Tan',
